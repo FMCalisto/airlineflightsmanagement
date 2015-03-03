2df175c18a3769a6282c9e6350678f82c04d2d0e
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-03-03 21:28:48 +0000

    Update files.

---
+++ src/main/java/pt/tecnico/phonebook/App.java
@@ -0,0 +1,13 @@
+package pt.tecnico.phonebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
